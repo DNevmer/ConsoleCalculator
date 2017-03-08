e42bb2fd1b4292213da47d4c9d208f3c61da99f6
DNevmer <dimitrijne@mail.ru>
2017-03-08 11:40:14 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    class Calculator
+    {
+        private List<string> operators = new List<string>(new string[] { "(", ")", "+", "-", "*", "/", "^" });
+
+        //разделение строки по элементам 
+        private List<string> Separate(string inputString)
+        {
+            List<string> separateStringList = new List<string>();
+            int position = 0;
+            while (position < inputString.Length)
+            {
+                if (inputString[position] == ' ')
+                    position++;
+                else
+                {
+                    string tempS = inputString[position].ToString();
+                    if (!operators.Contains(inputString[position].ToString()))
+                    {
+                        if (Char.IsDigit(inputString[position]))
+                            for (int i = position + 1; i < inputString.Length && (Char.IsDigit(inputString[i]) || inputString[i] == ',' || inputString[i] == '.'); i++)
+                            {
+                                if (inputString[i] == '.')
+                                    tempS += ',';
+                                else
+                                    tempS += inputString[i];
+                            }
+                    }
+                    separateStringList.Add(tempS);
+                    position += tempS.Length;
+                }
+            }
+            return separateStringList;
+        }
+
+        private byte Priority(string s)//приоритеты
+        {
+            switch (s)
+            {
+                case "(":
+                    return 0;
+                case ")":
+                    return 0;
+                case "+":
+                    return 1;
+                case "-":
+                    return 1;
+                case "*":
+                    return 2;
+                case "/":
+                    return 2;
+                case "^":
+                    return 3;
+                default:
+                    return 4;
+            }
+        }
+
+        //преобразование строки в обратную польскую запись 
+        private List<string> ConvertToPostfixNotation(string inputString)
+        {
+            List<string> postfixNotationList = new List<string>();
+            Stack<string> stack = new Stack<string>();
+            foreach (string s in Separate(inputString))
+            {
+                if (operators.Contains(s))
+                {
+                    if (stack.Count > 0 && !s.Equals("("))
+                    {
+                        if (s.Equals(")"))
+                        {
+                            string temp = stack.Pop();
+                            while (temp != "(")
+                            {
+                                postfixNotationList.Add(temp);
+                                temp = stack.Pop();
+                            }
+                        }
+                        else if (Priority(s) > Priority(stack.Peek()))
+                            stack.Push(s);
+                        else
+                        {
+                            while (stack.Count > 0 && Priority(s) <= Priority(stack.Peek()))
+                            {
+                                if (s.Equals("^") && stack.Peek() == "^")
+                                { break; }
+                                else
+                                    postfixNotationList.Add(stack.Pop());
+                            }
+                            stack.Push(s);
+                        }
+
+                    }
+                    else
+                        stack.Push(s);
+                }
+                else
+                    postfixNotationList.Add(s);
+            }
+            if (stack.Count > 0)
+                foreach (string s in stack)
+                    postfixNotationList.Add(s);
+
+            return postfixNotationList;
+        }
+
+        //вычисление выражения
+        public string Calculate(string inputString)
+        {
+            double rezult;
+            //List<string> postfixNotationList = ConvertToPostfixNotation(inputString);
+            Stack<double> stack = new Stack<double>();
+            foreach (string s in ConvertToPostfixNotation(inputString))
+            {
+                if (operators.Contains(s))
+                {
+
+                    switch (s)
+                    {
+                        case "+":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                double b = Convert.ToDouble(stack.Pop());
+                                rezult = a + b;
+                                stack.Push(rezult);
+                                break;
+                            }
+                        case "-":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                double b = Convert.ToDouble(stack.Pop());
+                                rezult = b - a;
+                                stack.Push(rezult);
+                                break;
+                            }
+                        case "*":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                double b = Convert.ToDouble(stack.Pop());
+                                rezult = a * b;
+                                stack.Push(rezult);
+                                break;
+                            }
+                        case "/":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                double b = Convert.ToDouble(stack.Pop());
+                                if (a == 0)//проверка деления на нуль
+                                    return "Divided by zero";
+                                rezult = b / a;
+                                stack.Push(rezult);
+                                break;
+                            }
+                        case "^":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                double b = Convert.ToDouble(stack.Pop());
+                                rezult = Math.Pow(b, a);
+                                stack.Push(rezult);
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    stack.Push(Convert.ToDouble(s));
+                }
+            }
+            return Convert.ToString(Math.Round(stack.Pop(),5));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            CalculateFile("input_1.txt", "output_1.txt");
+            CalculateFile("input_2.txt", "output_2.txt");
+        }
+
+        //считывание и запись в файл
+        public static void CalculateFile(string fileInputName, string fileOutputName)
+        {
+            Calculator calc = new Calculator();
+            string line;            
+            System.IO.StreamReader file = new System.IO.StreamReader(fileInputName);//считывание из файла
+            System.IO.File.Delete(fileOutputName);
+            while ((line = file.ReadLine()) != null)
+            {
+                System.IO.File.AppendAllText(fileOutputName, line + " = " + calc.Calculate(line) + Environment.NewLine);//вычисление и запись в файл
+            }
+        }
+    }
+}
