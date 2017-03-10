b9e0a6c6c39820abf954b2edcb8c22a7036b3b94
DNevmer <dimitrijne@mail.ru>
2017-03-10 15:55:42 +0300

    Add files via upload

--- Calculator.cs
+++ Calculator.cs
@@ -6,50 +6,72 @@ using System.Threading.Tasks;
 
 namespace ConsoleCalc
 {
+    public sealed class Operators
+    {
+        public static readonly Operators leftBracket = new Operators("(");
+        public static readonly Operators rightBracket = new Operators(")");
+        public static readonly Operators minus = new Operators("-");
+        public static readonly Operators plus = new Operators("+");
+        public static readonly Operators multiply = new Operators("*");
+        public static readonly Operators division = new Operators("/");
+        public static readonly Operators ex = new Operators("^");
+        public static readonly Operators cos = new Operators("cos");
+        public static readonly Operators sin = new Operators("sin");
+        public static readonly Operators tan = new Operators("tan");
+        public static readonly Operators sqrt = new Operators("sqrt");
+        public static readonly Operators comma = new Operators(",");
+
+        private Operators(string value)
+        {
+            Value = value;
+        }
+        public string Value { get; private set; }
+    }
+
     class Calculator
     {
-        private List<string> operators = new List<string>(new string[] { "(", ")", "+", "-", "*", "/", "^" });
+        private List<string> operators = new List<string>(new string[] { Operators.leftBracket.Value, Operators.rightBracket.Value, Operators.plus.Value, Operators.minus.Value, Operators.multiply.Value, Operators.division.Value, Operators.ex.Value });
+        private List<string> operatorsLetters = new List<string>(new string[] { Operators.cos.Value, Operators.sin.Value, Operators.tan.Value, Operators.sqrt.Value });
 
-        //разделение строки по элементам 
         private List<string> Separate(string inputString)
         {
             List<string> separateStringList = new List<string>();
             int position = 0;
-            bool flag = false;//флаг проверки унарного минуса
-            inputString = inputString.Replace(" ", String.Empty);//удаляет пробелы из входящей строки
+            bool flagUnaryMinus = false;
+
+            inputString = inputString.Replace(" ", String.Empty);
+            inputString = inputString.Replace(".", ",");
             while (position < inputString.Length)
-            {                
-                string tempS = inputString[position].ToString();
-                if ((inputString[position] == '-' && separateStringList.Count == 0) || (inputString[position] == '-' && inputString[position - 1] == '('))
-                {//проверка на унарный минус в начале выражения или после скобки                        
+            {
+                string tempString = inputString[position].ToString();
+                if ((String.Equals(inputString[position].ToString(), Operators.minus.Value) && separateStringList.Count == 0) || (String.Equals(inputString[position].ToString(), Operators.minus.Value) && String.Equals(inputString[position - 1].ToString(), Operators.leftBracket.Value)))
+                {//check unary minus                      
                     position++;
-                    tempS += inputString[position].ToString();
-                    flag = true;
+                    tempString += inputString[position].ToString();
+                    flagUnaryMinus = true;
                 }
                 if (!operators.Contains(inputString[position].ToString()))
                 {
                     if (Char.IsDigit(inputString[position]))
-                        for (int i = position + 1; i < inputString.Length && (Char.IsDigit(inputString[i]) || inputString[i] == ',' || inputString[i] == '.'); i++)
-                        {
-                            if (inputString[i] == '.')
-                                tempS += ',';
-                            else
-                                tempS += inputString[i];
-                        }
+                        for (int i = position + 1; i < inputString.Length && (Char.IsDigit(inputString[i]) || String.Equals(inputString[i].ToString(), Operators.comma.Value)); i++) 
+                                tempString += inputString[i];
+                    else if (char.IsLetter(inputString[position]))
+                        for (int i = position + 1; i < inputString.Length && Char.IsLetter(inputString[i]); i++)
+                            tempString += inputString[i];
                 }
-                separateStringList.Add(tempS);
-                if (flag)
+                separateStringList.Add(tempString);
+                if (flagUnaryMinus)
                 {
-                    position += tempS.Length - 1;
-                    flag = false;
+                    position += tempString.Length - 1;
+                    flagUnaryMinus = false;
                 }
                 else
-                    position += tempS.Length;
+                    position += tempString.Length;
             }
             return separateStringList;
         }
 
-        private byte Priority(string s)//приоритеты
+        private byte Priority(string s)
         {
             switch (s)
             {
@@ -72,115 +94,172 @@ namespace ConsoleCalc
             }
         }
 
-        //преобразование строки в обратную польскую запись 
         private List<string> ConvertToPostfixNotation(string inputString)
         {
             List<string> postfixNotationList = new List<string>();
             Stack<string> stack = new Stack<string>();
-            foreach (string s in Separate(inputString))
+            List<string> separateInputStringList = new List<string>(Separate(inputString));
+
+            for (int i = 0; i < separateInputStringList.Count; i++)
             {
-                if (operators.Contains(s))
+                if (operators.Contains(separateInputStringList[i]))
                 {
-                    if (stack.Count > 0 && !s.Equals("("))
+                    if (stack.Count > 0 && !separateInputStringList[i].Equals(Operators.leftBracket.Value))
                     {
-                        if (s.Equals(")"))
+                        if (separateInputStringList[i].Equals(Operators.rightBracket.Value))
                         {
                             string temp = stack.Pop();
-                            while (temp != "(")
+                            while (temp != Operators.leftBracket.Value)
                             {
                                 postfixNotationList.Add(temp);
                                 temp = stack.Pop();
                             }
                         }
-                        else if (Priority(s) > Priority(stack.Peek()))
-                            stack.Push(s);
+                        else if (Priority(separateInputStringList[i]) > Priority(stack.Peek()))
+                        {
+                            stack.Push(separateInputStringList[i]);
+                        }
                         else
                         {
-                            while (stack.Count > 0 && Priority(s) <= Priority(stack.Peek()))
+                            while (stack.Count > 0 && Priority(separateInputStringList[i]) <= Priority(stack.Peek()))
                             {
-                                if (s.Equals("^") && stack.Peek() == "^")
-                                { break; }
+                                if (separateInputStringList[i].Equals(Operators.ex.Value) && stack.Peek() == Operators.ex.Value)
+                                    break;
                                 else
                                     postfixNotationList.Add(stack.Pop());
                             }
-                            stack.Push(s);
+                            stack.Push(separateInputStringList[i]);
                         }
-
                     }
                     else
-                        stack.Push(s);
+                        stack.Push(separateInputStringList[i]);
+                }
+                else if (operatorsLetters.Contains(separateInputStringList[i]))
+                {                    
+                    Stack<string> bracketStack = new Stack<string>();
+                    string tempStringInBrackets = String.Empty;
+                    string tempOperatorLetters = separateInputStringList[i].ToString();
+                    i++;                    
+                    while (i < separateInputStringList.Count)//Generates a string in brackets
+                    {                   
+                        if (separateInputStringList[i].Equals(Operators.leftBracket.Value))
+                        {
+                            bracketStack.Push(separateInputStringList[i]);
+                            tempStringInBrackets += separateInputStringList[i].ToString();
+                            i++;
+                        }
+                        else if (separateInputStringList[i].Equals(Operators.rightBracket.Value))
+                        {
+                            bracketStack.Pop();
+                            tempStringInBrackets += separateInputStringList[i].ToString();                            
+                            if (bracketStack.Count == 0)
+                                break;
+                            i++;
+                        }
+                        else
+                        {
+                            tempStringInBrackets += separateInputStringList[i].ToString();
+                            i++;
+                        }
+                    }
+                    //recursive convert to postfix notation
+                    foreach (string tS in ConvertToPostfixNotation(tempStringInBrackets))
+                        postfixNotationList.Add(tS);
+                    postfixNotationList.Add(tempOperatorLetters);
                 }
                 else
-                    postfixNotationList.Add(s);
+                    postfixNotationList.Add(separateInputStringList[i]);
             }
             if (stack.Count > 0)
                 foreach (string s in stack)
                     postfixNotationList.Add(s);
-
             return postfixNotationList;
         }
 
-        //вычисление выражения
         public string Calculate(string inputString)
         {
-            double rezult;
-            //List<string> postfixNotationList = ConvertToPostfixNotation(inputString);
+            double result;
             Stack<double> stack = new Stack<double>();
             foreach (string s in ConvertToPostfixNotation(inputString))
             {
-                if (operators.Contains(s))
+                if (operators.Contains(s)  || operatorsLetters.Contains(s))
                 {
-
                     switch (s)
                     {
                         case "+":
                             {
                                 double a = Convert.ToDouble(stack.Pop());
                                 double b = Convert.ToDouble(stack.Pop());
-                                rezult = a + b;
-                                stack.Push(rezult);
+                                result = a + b;
+                                stack.Push(result);
                                 break;
                             }
                         case "-":
                             {
                                 double a = Convert.ToDouble(stack.Pop());
                                 double b = Convert.ToDouble(stack.Pop());
-                                rezult = b - a;
-                                stack.Push(rezult);
+                                result = b - a;
+                                stack.Push(result);
                                 break;
                             }
                         case "*":
                             {
                                 double a = Convert.ToDouble(stack.Pop());
                                 double b = Convert.ToDouble(stack.Pop());
-                                rezult = a * b;
-                                stack.Push(rezult);
+                                result = a * b;
+                                stack.Push(result);
                                 break;
                             }
                         case "/":
                             {
                                 double a = Convert.ToDouble(stack.Pop());
                                 double b = Convert.ToDouble(stack.Pop());
-                                if (a == 0)//проверка деления на нуль
+                                if (a == 0)
                                     return "Divided by zero";
-                                rezult = b / a;
-                                stack.Push(rezult);
+                                result = b / a;
+                                stack.Push(result);
                                 break;
                             }
                         case "^":
                             {
                                 double a = Convert.ToDouble(stack.Pop());
                                 double b = Convert.ToDouble(stack.Pop());
-                                rezult = Math.Pow(b, a);
-                                stack.Push(rezult);
+                                result = Math.Pow(b, a);
+                                stack.Push(result);
+                                break;
+                            }
+                        case "cos":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                result = Math.Cos(a);
+                                stack.Push(result);
+                                break;
+                            }
+                        case "sin":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                result = Math.Sin(a);
+                                stack.Push(result);
+                                break;
+                            }
+                        case "tan":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                result = Math.Tan(a);
+                                stack.Push(result);
+                                break;
+                            }
+                        case "sqrt":
+                            {
+                                double a = Convert.ToDouble(stack.Pop());
+                                result = Math.Sqrt(a);
+                                stack.Push(result);
                                 break;
                             }
                     }
                 }
                 else
-                {
                     stack.Push(Convert.ToDouble(s));
-                }
             }
             return Convert.ToString(Math.Round(stack.Pop(),5));
         }
--- Program.cs
+++ Program.cs
@@ -9,22 +9,22 @@ namespace ConsoleCalc
     class Program
     {
         static void Main(string[] args)
-        {     
+        {
             CalculateFile("input_1.txt", "output_1.txt");
             CalculateFile("input_2.txt", "output_2.txt");
+            CalculateFile("input_3.txt", "output_3.txt");
         }
 
-        //считывание и запись в файл
+        //read file, calculate and write result in file
         public static void CalculateFile(string fileInputName, string fileOutputName)
         {
             Calculator calc = new Calculator();
-            string line;            
-            System.IO.StreamReader file = new System.IO.StreamReader(fileInputName);//считывание из файла
+                       
+            System.IO.StreamReader file = new System.IO.StreamReader(fileInputName);
             System.IO.File.Delete(fileOutputName);
+            string line; 
             while ((line = file.ReadLine()) != null)
-            {
-                System.IO.File.AppendAllText(fileOutputName, line + " = " + calc.Calculate(line) + Environment.NewLine);//вычисление и запись в файл
-            }
+                System.IO.File.AppendAllText(fileOutputName, line + " = " + calc.Calculate(line) + Environment.NewLine);
         }
     }
 }
